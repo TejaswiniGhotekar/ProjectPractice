52a590456aee2d59954b28d829c09aef721423e0
tshivajighoteka <tshivajighoteka@L-INE-2P1LLW2.ardianet.net>
2024-01-23 17:55:16 +0530

    adding logical prog of number triangle

--- src/test/java/Expleopractice24/Automation_exp/testClass.java
+++ src/test/java/Expleopractice24/Automation_exp/testClass.java
@@ -3,11 +3,21 @@ package Expleopractice24.Automation_exp;
 public class testClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		int a = 10;
-		int b = 8;
-		System.out.println(a+b);
+		int a = 1;
+		for(int i=0; i<4; i++) {
+			
+			for(int j=1; j<=4-i;j++) {
+				
+				System.out.print(a + " ");
+				a++;
+				
+				}
+			System.out.println();
+			
+		}
+		}
+
 
 	}
 
-}
+
